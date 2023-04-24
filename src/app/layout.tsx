8888f7bddec29130e538c5e91486f5f24b7915d7
ortizdaniel <brisk.salt4034@fastmail.com>
2023-04-24 23:19:51 +0200

    use chakra ui (#4)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ['latin'] })
+ // import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Boigs',
-}
+  title: "Boigs",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
-  )
+  );
 }
