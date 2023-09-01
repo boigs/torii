bb751d294ff7e019e8d177c0f2cabb35349f0ab6
Sergi Valbuena Garrido <24233455+svalbuena@users.noreply.github.com>
2023-09-01 17:28:40 +0200

    Fix Tailwind and ChakraUI integration (#7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import './globals.css';
 import { Providers } from './providers';
-
-// import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Boigs',
+  description: 'no',
 };
 
 export default function RootLayout({
