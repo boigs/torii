28412690b07e6ae351e7f734bc66a2ff139b362a
Daniel <brisk.salt4034@fastmail.com>
2023-09-03 15:44:54 +0200

    remove extra flex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,13 @@
 
 import React from 'react';
 
-import { Center, Flex } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
 import HostForm from 'src/components/HostForm/HostForm';
 
 const Home: React.FC = () => {
   return (
     <Center>
-      <Flex gap='12px' alignItems='flex-start'>
-        <Flex flexDirection='column' gap='12px'>
-          <HostForm />
-        </Flex>
-      </Flex>
+      <HostForm />
     </Center>
   );
 };
