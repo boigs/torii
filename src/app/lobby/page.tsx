1af20e84be7ed2fa2a645f0fdd2a2e1d029a3474
svalbuena <24233455+svalbuena@users.noreply.github.com>
2023-09-05 22:41:44 +0200

    add ws

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -1,15 +1,30 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Center } from '@chakra-ui/react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+import { Button, Center } from '@chakra-ui/react';
 
 import PlayerList from 'src/components/PlayerList/PlayerList';
 
 const Lobby: React.FC = () => {
+  const [socketUrl, setSocketUrl] = useState('ws://localhost:4000/ws/game/A/player/dani');
+  const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>([]);
+  const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
+
+  useEffect(() => {
+    if (lastMessage !== null) {
+      console.log("last message: " + lastMessage);
+      console.log(lastMessage);
+      setMessageHistory((prev) => prev.concat(lastMessage));
+    }
+  }, [lastMessage, setMessageHistory]);
+
+  const handleClickSendMessage = () => sendMessage('Hello');
+
   return (
     <Center>
-      <PlayerList />
+      <Button onClick={handleClickSendMessage}>Send WS Message</Button>
     </Center>
   );
 };
