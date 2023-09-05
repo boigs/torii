5f9e6e0f164d726feca926a6ae9be63139544e3b
svalbuena <24233455+svalbuena@users.noreply.github.com>
2023-09-05 22:45:46 +0200

    fix prettier

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 import { Button, Center } from '@chakra-ui/react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
 
 import PlayerList from 'src/components/PlayerList/PlayerList';
 
