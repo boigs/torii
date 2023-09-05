bb9db65dfc2b12657f309e1fd14d57136f87f5af
svalbuena <24233455+svalbuena@users.noreply.github.com>
2023-09-05 22:44:43 +0200

    format

--- src/app/lobby/page.tsx
+++ src/app/lobby/page.tsx
@@ -1,20 +1,22 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-
 import useWebSocket, { ReadyState } from 'react-use-websocket';
+
 import { Button, Center } from '@chakra-ui/react';
 
 import PlayerList from 'src/components/PlayerList/PlayerList';
 
 const Lobby: React.FC = () => {
-  const [socketUrl, setSocketUrl] = useState('ws://localhost:4000/ws/game/A/player/dani');
+  const [socketUrl, setSocketUrl] = useState(
+    'ws://localhost:4000/ws/game/A/player/dani'
+  );
   const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>([]);
   const { sendMessage, lastMessage, readyState } = useWebSocket(socketUrl);
 
   useEffect(() => {
     if (lastMessage !== null) {
-      console.log("last message: " + lastMessage);
+      console.log('last message: ' + lastMessage);
       console.log(lastMessage);
       setMessageHistory((prev) => prev.concat(lastMessage));
     }
