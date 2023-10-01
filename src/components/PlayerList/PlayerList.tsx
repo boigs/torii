3b970894e2d3f0ff277b5a28783071cf185f4816
svalbuena <24233455+svalbuena@users.noreply.github.com>
2023-10-01 10:26:45 +0200

    Copy

--- src/components/PlayerList/PlayerList.tsx
+++ src/components/PlayerList/PlayerList.tsx
@@ -20,7 +20,7 @@ const PlayerList: React.FC<PlayerListProps> = ({ players }) => {
   return (
     <Card size='sm' width='sm'>
       <CardHeader paddingBottom={0}>
-        <Heading as='h3' textAlign='center' size='md'>
+        <Heading as='h4' textAlign='center' size='sm'>
           Players ({players.length})
         </Heading>
         <Divider marginTop={'12px'} />
