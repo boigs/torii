90f2209f5a8140d7ad549de465e76423aa8e22ab
Daniel <brisk.salt4034@fastmail.com>
2023-10-08 19:39:13 +0200

    feat: toast errors

--- src/app/game/[id]/page.tsx
+++ src/app/game/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
 import {
   Button,
@@ -13,9 +13,13 @@ import {
   Input,
   VStack,
   useClipboard,
+  useToast,
 } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import { join } from 'path';
 import useWebSocket from 'react-use-websocket';
 
+import { PlayerProps } from 'src/components/PlayerList/Player';
 import PlayerList from 'src/components/PlayerList/PlayerList';
 import config from 'src/config';
 
@@ -25,23 +29,78 @@ type GameQuery = {
   };
 };
 
+enum Type {
+  Error = 'Error',
+  GameState = 'GameState',
+}
+
+type MessageType = {
+  type: Type;
+};
+
+type HeadcrabError = {
+  message: string;
+};
+
+type GameState = {
+  players: PlayerProps[];
+};
+
+type Message = MessageType & (GameState | HeadcrabError);
+
 const Game: React.FC<GameQuery> = ({ params: { id } }) => {
+  const toast = useToast();
+  const router = useRouter();
+
   const nickname = localStorage.getItem('nickname');
   const joinUrl = `${window.location.origin}/join/${id}`;
+  const [players, setPlayers] = useState<{ nickname: string }[]>([]);
+  const { onCopy, hasCopied } = useClipboard(joinUrl);
+
   const [socketUrl, _] = useState(
     `${config.headcrabWsBaseUrl}/game/${id}/player/${nickname}/ws`
   );
-  const [players, setPlayers] = useState<{ nickname: string }[]>([]);
-  const { sendMessage, lastMessage } = useWebSocket(socketUrl, { share: true });
-  const { onCopy, hasCopied } = useClipboard(joinUrl);
+
+  const onWebsocketError: (e: Event) => void = useCallback(
+    (e) => {
+      console.log('WS Error', e);
+      toast({
+        status: 'error',
+        isClosable: true,
+        duration: 5000,
+        description: 'Unknown error occurred; please contact support.',
+        position: 'top',
+      });
+    },
+    [toast]
+  );
+  const { sendMessage, lastMessage } = useWebSocket(socketUrl, {
+    onError: onWebsocketError,
+  });
 
   useEffect(() => {
     if (lastMessage !== null) {
       console.log(lastMessage);
-      const message = JSON.parse(lastMessage.data);
-      setPlayers(message.players);
+      const wsMessage: Message = JSON.parse(lastMessage.data);
+      switch (wsMessage.type) {
+        case Type.GameState:
+          const { players } = wsMessage as GameState;
+          setPlayers(players);
+          break;
+        case Type.Error:
+          const { message } = wsMessage as HeadcrabError;
+          toast({
+            status: 'error',
+            isClosable: true,
+            duration: 5000,
+            description: message,
+            position: 'top',
+          });
+          router.push(joinUrl);
+          break;
+      }
     }
-  }, [lastMessage]);
+  }, [lastMessage, toast, router, joinUrl]);
 
   return (
     <Center>
