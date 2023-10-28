0a8c633d4c2ca7cfb32804d31162300714444238
ortizdaniel <brisk.salt4034@fastmail.com>
2023-10-28 08:51:29 +0200

    chore: refactor fsm (#53)

--- src/state/GameContext/gameState.ts
+++ src/state/GameContext/gameState.ts
@@ -1,106 +1,91 @@
-import { useMachine } from '@xstate/react';
-import { createMachine } from 'xstate';
-import config from 'src/config';
-import { assign } from 'lodash';
+import { assign, createMachine } from 'xstate';
 
-enum State {
-  DISCONNECTED = 'disconnected',
-  CREATING_GAME = 'creatingGame',
-  JOINING_GAME = 'joiningGame',
-  LOBBY = 'lobby',
-}
+type CreateGameEvent = {
+  type: 'CREATE_GAME';
+  value: {
+    nickname: string;
+  };
+};
 
-enum Event {
-  CREATE_GAME = 'CREATE_GAME',
-  JOIN_GAME = 'JOIN_GAME',
-}
+type JoinGameEvent = {
+  type: 'JOIN_GAME';
+  value: {
+    gameId: string;
+    nickname: string;
+  };
+};
 
-enum Services {
-  CREATE_GAME = "createGame",
-  JOIN_GAME = "joinGame"
-}
+type CreateGameService = {
+  data: {
+    gameId: string;
+  };
+};
 
-enum Action {
-  ASSIGN_PLAYER_DETAILS_CONTEXT = "assignPlayerDetailsContext"
-}
+type JoinGameService = {
+  data: {};
+};
 
-const gameMachine = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5QAoC2BDAxgCwJYDswBKAOgl1kwHt9DMAXSAYgGEAlAUQEEAVDgfQDiXALIcA2gAYAuolAAHKrFz1cNOSAAeiAEw7JJAIwAWABymA7MckA2C7Zu2ANCACeugKwejHm3smSHgCcQaZBOgC+ES5oWHiEpOSUNHSMEEwAUgDyAJIAckKiEjIaisqq6khauvpGZpbWdg7ObogAzDbepo06hh7GJsZeNlExGDgExCSYAE5g6Kr4UILoqGBMEDRgJAQAblQA1tuxEwnTcwsEy6tgCHtUmJc0UtIvpUoqavga2giOpiQPIYgoZbKC2qYdKEXO4EHpjCQLMEgsY2r19AFIaMQCd4lNZvNFtc1kwwDMZlQZiR5AAbBYAM0pqBIuMmpAJlyWKzWd3w+0eFXwLzeVTKn0qoF+-jq5istnsNkcNhhukkAKCAQ8FjaHnRSOM2NZZwAVlQCFduetNoQdnzDsdxnjSKbzVybrz+U8hTIRQoPoKfohQgYLJ1gQ4bMYdBYgiq4dGSDpkTo2hZTG0gl4U4bHWySC78BabqTyZTqXT6IyZsyjVMC0WefcBV9hSVRf6voG-gm2m0BqY-B5JFYdXHTIYjOPTB4dXZLJJDJFsfgqBA4Bpa0R3uVO1VfgBaZWtBCHkgBc8Xy8WHNxPNJai0MAMSDb8XfPeIKNxjUkGyLwwWAB5jGAMII3qc+IXESlqvgGH4INYOhnhmw4DIqUJ9nGwJBCQpiSEEnRyiC45DOBTr5mahZumssG7pKiAxhO+ppoqNhhIYYRxvoSFtPhhHWMRJgeGReY0lQABG4mwn6O4StUCCGCYNiAm0hgQkMA4dJGXFJr+IHDhxWpeCBphRFEQA */
-  id: 'game',
-  tsTypes: {} as import("./gameState.typegen").Typegen0,
-  schema: {
-    events: {} as
-    | {
-        type: Event.CREATE_GAME,
-        value: string
-    }
-    | {
-        type: Event.JOIN_GAME,
-        value: string
-    }
-    ,
-    services: {} as {
-      [Services.CREATE_GAME]: {
-        data: string
-      },
-      [Services.JOIN_GAME]: {
-        data: string
-      }
-    }
-  },
-  context: {
-    playerDetails: {} as { gameId: string, nickname: string }
-  },
-  initial: State.DISCONNECTED,
-  states: {
-    [State.DISCONNECTED]: {
-      on: {
-        // When sending these two events, send the nickname and use an action to store it on the context
-        [Event.CREATE_GAME]: [{target: State.CREATING_GAME, actions: 'assignNicknameToContext'}],
-        [Event.JOIN_GAME]: State.JOINING_GAME,
+const gameMachine = createMachine(
+  {
+    /** @xstate-layout N4IgpgJg5mDOIC5RQIYFswDoIEtYGMB7AO2LHwBdIBiAYQCUBRAQQBVGB9AcWYFlGA2gAYAuolAAHQrBwUcJcSAAeiAEwAOAMyYAnOtWqALKoDsARnUBWMydUAaEAE9EO3TstDPhszdM6fAL4BDqgY2HhEpORUENQAUgDyAJIActx8gqKKUjJyCkjKiIYmJpgAbJomQjqq7iZ6Bg7OCGaalphVOm3+3ma1OkEh6Fj4AE5gKHLEUFzD1BAkWDjEAG6EANZYoSPjk8szwwjLa-h7JMIiF9nSsvLEiioIptplFqqWpmWWZSbemk2INraEqqTRCHxmQyaL6WQYgbaYMYTKYHDDUMCjUaEUaYCQAG0mADNsWhMAikXtprMMEdVoRTnliBcrgUcrd8qBHq9MD5NGZLJZNOp1J0dIYAQhDK5vIL+SYyoZLPUymU4QiAFaEZb7algeaLTDHDZbYaYTXaqmHI0Mu7MrKsm6Mh6IMo6ISYdQWKHFEzqKHWCWaMEdOqqV5+kxtNWm83EHVzDFYnH4okkskxrVxy0061nJmiFmSR13Z0IQWuMFQvSWfTVf5ORAAWn8HXe6nc3jKnqBQWCIGIhAgcEU22uuRLBUerR5rX5guFou8EsbNlcBks-iD84M8ujYVwBBIZEokDH7Puk8BgswhmqAu7bS+4obCEb+g9lilqkhUK0Xj3OzIvGGBnk6l5PF8uieN+pgmBuNhmBKZgVJgHxwRCmhdF2PwAWambAWAoETpyRTyje-gWGKVQqloZSBhYmDgmU7wfEIlQfKqfYInihAAEa8c0RbjhyhQILYhjlJGwpfH07wmEh6juoYwrGJG-RaIYvYBEAA */
+    id: 'game',
+    tsTypes: {} as import('./gameState.typegen').Typegen0,
+    schema: {
+      events: {} as CreateGameEvent | JoinGameEvent,
+      services: {} as {
+        createGame: CreateGameService;
+        joinGame: JoinGameService;
       },
     },
-    [State.CREATING_GAME]: {
-      invoke: {
-        src: 'createGame',
-        onDone: State.JOINING_GAME,
-        onError: State.DISCONNECTED,
+    context: {
+      gameId: undefined as string | undefined,
+      nickname: undefined as string | undefined,
+      lastMessage: undefined as string | undefined,
+    },
+    initial: 'disconnected',
+    states: {
+      disconnected: {
+        on: {
+          // When sending these two events, send the nickname and use an action to store it on the context
+          CREATE_GAME: { target: 'creatingGame', actions: 'assignNickname' },
+          JOIN_GAME: {
+            target: 'joiningGame',
+            actions: ['assignGameId', 'assignNickname'],
+          },
+        },
       },
-    },
-    [State.JOINING_GAME]: {
-      invoke: {
-        src: Services.JOIN_GAME,
-        onDone: {target: State.LOBBY, actions: 'assignPlayerDetailsToContext'},
-        onError: State.DISCONNECTED,
+      creatingGame: {
+        entry: [],
+        invoke: {
+          src: 'createGame',
+          onDone: { target: 'joiningGame', actions: ['assignGameId'] },
+          onError: 'disconnected',
+        },
       },
+      joiningGame: {
+        invoke: {
+          src: 'joinGame',
+          onDone: 'lobby',
+          onError: 'disconnected',
+        },
+      },
+      lobby: {},
     },
-    [State.LOBBY]: {},
   },
-},
-{
-  actions: {
-    assignPlayerDetailsToContext: assign((context, event) => {
-      return {
-        playerDetails: "a"
-      }
-    }),
-    assignNicknameToContext: assign((context, event) => {
-      return {
-      }
-    })
-  },
-});
-
-const [state, send] = useMachine(gameMachine, {
-  services: {
-    [Services.CREATE_GAME]: async () => {
-      let gameId = await fetch(`${config.headcrabHttpBaseUrl}/game`, {
-        method: 'POST',
-      })
-        .then((response: any) => response.json())
-        .then((response: any) => response.id);
-      return gameId;
+  {
+    actions: {
+      assignNickname: assign((_, event) => {
+        return { nickname: event.value.nickname };
+      }),
+      assignGameId: assign((_, event) => {
+        let gameId: string;
+        if (event.type === 'JOIN_GAME') {
+          gameId = (event as JoinGameEvent).value.gameId;
+        } else {
+          gameId = event.data.gameId;
+        }
+        return { gameId };
+      }),
     },
-    [Services.JOIN_GAME]: async (context, event) => {},
   }
-});
+);
