608818ae7ebbe9677f1da27d0e1fd3e40b09ea07
svalbuena <24233455+svalbuena@users.noreply.github.com>
2023-10-29 15:20:20 +0100

    merge conflict

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['api.dicebear.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: ''
+      }
+    ],
   },
 };
 
