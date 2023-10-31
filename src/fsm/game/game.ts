533fda4dcf4cf71b0a0878c61860cf113aff3058
ortizdaniel <brisk.salt4034@fastmail.com>
2023-10-31 19:56:44 +0100

    chore: remove inlined funcs (#60)

--- src/fsm/game/game.ts
+++ src/fsm/game/game.ts
@@ -38,92 +38,121 @@ type Context = {
   players: Player[];
 };
 
-const createGame = () =>
+const createGame: () => Promise<string> = () =>
   fetch(`${config.headcrabHttpBaseUrl}/game`, { method: 'POST' })
     .then((response) => response.json())
     .then((response) => (response as { id: string }).id);
 
-const gameFsm = createMachine({
-  /** @xstate-layout N4IgpgJg5mDOIC5RQIYFswDoIEtYGMB7AO2LHwBdIBiAYQCUBRAQQBVGB9AcWYFlGA2gAYAuolAAHQrBwUcJcSAAeiAEwB2dZgAsAVgBsADlWHtQgMzb1Q-QBoQATzXnMARn2Whp7eYCcq1R8AXyD7VAxsPCJScioIagApAHkASQA5bj5BUUUpGTkFJGVEbT1MX3MNX3VtX1dfau17JwRtQ0whf0NDISFXXXUe9X0QsPQsfAAnMBQ5YiguceoIEiwcYgA3QgBrLHCJ6dn1hfGEda38I5JhERvc6Vl5YkUVBFV9LU6Td+ttetdXOpmohzPp9JgPK5OgCPOpLOZ1KMQPtMFMZnMThhqGBJpNCJNMBIADazABm+LQmBRaKO80WGDOm0IlwKxBudyKeUehVAr102kwukMmlUFQ0AX01WBCH0qkwGj+QiFrj8bTaSJRACtCOtjvSwMtVphzjs9uNMNrdXTTiaWU92TlOQ9WS9EEYBaLzL1he5fLoAtL4RD+bp3KpLFCEYjQsjzZbiHqlji8QTiWSKVS4zqE9aGbarmzRBzJM6nq6ELpKphNIZqqp-TYoa5pQBaQaYQwfKGVj6lKyuEIx4iECBwRT7e75MtFV7goUisXqCW+D6t9zg0HqKE1fR6AYGDXm3AEEhkSiQSfc54zkHacHdP6VIRLryBVuS8orlUq3ylfSuAJDwiGkMX1S8XRvN5dF8TAESsTst1KCNdGlQFwUlaD-i3AxKiArB40TDBwOnXkSg-FcBg8Mw6kMflAy9Dt1F0IQ9HrcwANcKw8MwIlCAAIz4loSynHligQLd2k40x9BY5jND+VCKlgqxVEBBEKn9cNByCIA */
-  id: 'game',
-  predictableActionArguments: true,
-  tsTypes: {} as import('./game.typegen').Typegen0,
-  schema: {
-    events: {} as
-      | CreateGameEvent
-      | JoinGameEvent
-      | WebsocketConnectErrorEvent
-      | WebsocketMessageEvent,
-    context: {} as Context,
-  },
-  context: {
-    gameId: undefined,
-    nickname: undefined,
-    players: [],
-  },
-  initial: 'disconnected',
-  states: {
-    disconnected: {
-      on: {
-        CREATE_GAME: {
-          target: 'creatingGame',
-          actions: assign({ nickname: (_, event) => event.value.nickname }),
-        },
-        JOIN_GAME: {
-          target: 'joiningGame',
-          actions: assign({
-            nickname: (_, event) => event.value.nickname,
-            gameId: (_, event) => event.value.gameId,
-          }),
-        },
+const gameFsm = createMachine(
+  {
+    /** @xstate-layout N4IgpgJg5mDOIC5RQIYFswDoIEtYGMB7AO2LHwBdIBiAYQCUBRAQQBVGB9AcWYFlGA2gAYAuolAAHQrBwUcJcSAAeiAEwB2dZgAsAVgBsADlWHtQgMzb1Q-QBoQATzXnMARn2Whp7eYCcq1R8AXyD7VAxsPCJScioIagApAHkASQA5bj5BUUUpGTkFJGVEK11MX1dzQyqPD3VzVXsnBHVVTED1bX1tQ2qhIV11QxCw9Cx8ACcwFDliKC4x6ggSLBxiADdCAGsscPGpmbX5sYQ1zfxDkmERa9zpWXliRRUEVV02oVdXQyFfKsrDL5fIYmohLK5MOYhKpuup9EZdH4hOoRiA9phJtNZscMNQwBMJoQJpgJAAbGYAMyJaEw6MxhzmCwwpw2hAuBWI11uRTyD0KoBeum0mF0hk0qj+GgC+l86lBCH0bQ02lcA0MlV8PR6qPRACtCGsjkywNQAOqMABCAGUkrQANKMVgcfhWq3MLjZMQ8+4c56IdSuYW6fxVLy6ZH+OWORBCTCaFVmOHGVxwirmHVjTD6w2Mxbm622h1Ol1uj0CVxeyQ+x5+hWqCGmcyuXRfbQaMXmeUAWl8scs9YCP1FKd8ugzEWzxCNectNvtjo4tCSaTSjFoTsY9HoSXo3Kr+RrRReAZcAXMg2Dv2+ul08rb+nKb375nUf3eqnToTRmdJhAARn+DhmrOhYLiW7qencB78sUCC9GU4YGJqTb6F81TyjCEKuB+3z6K+-iih445YL+AFAfmc5Fs6jCuhB5aViAvK+keiCAi4-jfBxOHBiC0YINhEL9CY3yfE2fjvCEX7EIQEBwIoexQXyTwsQqIpiq0kqtDCvh4d2KZaKY9YWOoBitNo2jEZEBAkGQlCQIpzECmCXSYL0KoNMiqheIE3YyuUOmVBq5moQEln0tixoOYeTmvMGkKdEMeGBj4Kq3nxKYPjKwauBUAYGA0lmTtOGBRTBgqqq5VTnl0pkVGlzRQi46pfH8PSBqKFlfuipGAaVykxQGhhuFq+hCHoyKdK48o5S4litCmL5vmeklBEAA */
+    id: 'game',
+    predictableActionArguments: true,
+    tsTypes: {} as import('./game.typegen').Typegen0,
+    schema: {
+      events: {} as
+        | CreateGameEvent
+        | JoinGameEvent
+        | WebsocketConnectErrorEvent
+        | WebsocketMessageEvent,
+      context: {} as Context,
+      services: {} as {
+        createGame: {
+          data: { gameId: string };
+        };
       },
     },
-    creatingGame: {
-      invoke: {
-        src: () => createGame(),
-        onDone: {
-          target: 'joiningGame',
-          actions: [assign({ gameId: (_, event) => event.data })],
+    context: {
+      gameId: undefined,
+      nickname: undefined,
+      players: [],
+    },
+    initial: 'disconnected',
+    states: {
+      disconnected: {
+        on: {
+          CREATE_GAME: {
+            target: 'creatingGame',
+            actions: 'assignNickname',
+          },
+          JOIN_GAME: {
+            target: 'joiningGame',
+            actions: ['assignNicknameAndGameId'],
+          },
         },
-        onError: 'disconnected',
       },
-    },
-    joiningGame: {
-      entry: async (context, event) => {
-        console.log('probando');
-        console.log(context);
+      creatingGame: {
+        invoke: {
+          src: 'createGame',
+          onDone: {
+            target: 'joiningGame',
+            actions: 'assignGameId',
+          },
+          onError: 'disconnected',
+        },
       },
-      on: {
-        WEBSOCKET_MESSAGE: [
-          {
-            cond: (_, event) => event.value.message.type === WsType.GameState,
-            target: 'lobby',
-            actions: assign({
-              players: (_, event) => (event.value.message as GameState).players,
-            }),
-          },
-          'disconnected',
-        ],
-        WEBSOCKET_CONNECT_ERROR: 'disconnected',
+      joiningGame: {
+        on: {
+          WEBSOCKET_MESSAGE: [
+            {
+              cond: 'isGameStateMessage',
+              target: 'lobby',
+              actions: 'assignPlayers',
+            },
+            'disconnected',
+          ],
+          WEBSOCKET_CONNECT_ERROR: 'disconnected',
+        },
       },
-    },
-    lobby: {
-      on: {
-        WEBSOCKET_MESSAGE: [
-          {
-            cond: (_, event) => event.value.message.type === WsType.GameState,
-            actions: assign({
-              players: (_, event) => (event.value.message as GameState).players,
-            }),
-          },
-          {
-            cond: (_, event) => event.value.message.type === WsType.Error,
-            target: 'disconnected',
-          },
-        ],
+      lobby: {
+        on: {
+          WEBSOCKET_MESSAGE: [
+            {
+              cond: 'isGameStateMessage',
+              actions: 'assignPlayers',
+            },
+            {
+              cond: 'isErrorMessage',
+              target: 'disconnected',
+            },
+          ],
+        },
       },
     },
   },
-});
+  {
+    actions: {
+      assignNickname: assign((_, event) => ({
+        nickname: event.value.nickname,
+      })),
+      assignGameId: assign((_, event) => ({
+        gameId: event.data.gameId,
+      })),
+      assignNicknameAndGameId: assign((_, event) => ({
+        gameId: event.value.gameId,
+        nickname: event.value.nickname,
+      })),
+      assignPlayers: assign((_, event) => {
+        const { players } = event.value.message as GameState;
+        return {
+          players,
+        };
+      }),
+    },
+    services: {
+      createGame: async () => {
+        const gameId = await createGame();
+        return {
+          gameId,
+        };
+      },
+    },
+    guards: {
+      isGameStateMessage: (_, event) =>
+        event.value.message.type === WsType.GameState,
+      isErrorMessage: (_, event) => event.value.message.type === WsType.Error,
+    },
+  }
+);
 
 export default gameFsm;
