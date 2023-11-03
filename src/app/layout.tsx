6a8d0ac880dcf0e006e2607e0db95062c3b4153b
Daniel <brisk.salt4034@fastmail.com>
2023-11-03 23:47:07 +0100

    chore: v2 favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,29 +23,30 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <link
-          rel='icon'
-          href='/favicon.ico?v=2'
-        />
+        <link rel='icon' href='/favicon.ico?v=2' />
         <link
           rel='apple-touch-icon'
           sizes='180x180'
-          href='/apple-touch-icon.png'
+          href='/apple-touch-icon.png?v=2'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='32x32'
-          href='/favicon-32x32.png'
+          href='/favicon-32x32.png?v=2'
         />
         <link
           rel='icon'
           type='image/png'
           sizes='16x16'
-          href='/favicon-16x16.png'
+          href='/favicon-16x16.png?v=2'
         />
         <link rel='manifest' href='/site.webmanifest' />
-        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#bee3f8' />
+        <link
+          rel='mask-icon'
+          href='/safari-pinned-tab.svg?v=2'
+          color='#bee3f8'
+        />
         <meta name='msapplication-TileColor' content='#bee3f8' />
         <meta name='theme-color' content='#bee3f8' />
       </head>
