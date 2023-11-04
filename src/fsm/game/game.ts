f431c73a23e0c4cbaf7a2a55a05877fae14ca6df
Daniel <brisk.salt4034@fastmail.com>
2023-11-04 10:06:34 +0100

    feat: better gap and reset event

--- src/fsm/game/game.ts
+++ src/fsm/game/game.ts
@@ -32,6 +32,10 @@ type WebsocketConnectErrorEvent = {
   type: 'WEBSOCKET_CONNECT_ERROR';
 };
 
+type ResetEvent = {
+  type: 'RESET';
+};
+
 type Context = {
   gameId: string;
   nickname: string;
@@ -45,7 +49,7 @@ const createGame: () => Promise<string> = () =>
 
 const gameFsm = createMachine(
   {
-    /** @xstate-layout N4IgpgJg5mDOIC5RQIYFswDoIEtYGMB7AO2LHwBdIBiAYQCUBRAQQBVGB9AcWYFlGA2gAYAuolAAHQrBwUcJcSAAeiAOwA2AIyZVAJgCsmzboDMAFl3qNADgA0IAJ6J9J-Zk379Q60I0H1AJyqAL7B9qgY2HhEpORUENQAUgDyAJIActx8gqKKUjJyCkjKzt6YWgHGqhpCAUK6GvZOCGZCZpi1WpqqFtZmZgFmoeHoWPgATmAocsRQXKPUECRYOMQAboQA1lgRY5PTq3OjCKsb+AckwiJXedKy8sSKKgj6qtommmYmQSau6roBXRNZyWTDWcy6TT1X6aayQ4YgXaYCZTGZHDDUMDjcaEcaYCQAG2mADNcWhMEiUQdZvMMCd1oRzoViFcbsV8vciqBnuDVDp9NZ1OYrIKLEDHIghXzuoLKroBvp1PUEUiAFaEVaHWlgagAdUYACEAMrJWgAaUYrA4tGS6XSjFoVsY9HoyXobMkd2ZT0QJiEQkwFleJlUAWsnjqAWBCFcfNavy8fTM+la+hVo0w6s1NIW+uNpotVv4RqNzC4OTE7K9Dx9CAAtFD2t1PqoBVprMZoynrJgPr49KoYXDNOnIlniFrc4aTebLRxi6XywJNJXPQUa8VnnXzL3IbVqmZ1ODYZpo4eAph9PKrAZNL8BofR1gCYQAEavhx66cFucLssV251y5Ep63UMxtEPRVwKEFw6ksaMwIDcDBWMa91AFIYwkRDMX3fT88xnQt50YEt-2XVcQA5b1N0QOtDxMdx5W+SxTE+AITDPQJL2vPRDHvQZQiw4hCAgOBFF2QDOUeGjQOsC9ILAqFYPqdRo0hdRL1DT5jDhLQtF0J8ogIEgyEoSBJOo7lEA7dpdGvPsvAacFoz9dpfEqL5AThaxVF8QyqTRbULI3KyEEHbRqhgq8QyFXToz0BilV8sN1C0AVdGsQzx0nDBguA5540wdiPH6TRAnlawfK7Mwez7aoGiHeEsKRXCPzy6TQu3YxuMGSwhWML4PGq3RyjDVpKv6JU-MEoA */
+    /** @xstate-layout N4IgpgJg5mDOIC5RQIYFswGIBKBRAyrgCoDaADALqKgAOA9rAJYAujdAdtSAB6IAcAZj4A6MmQCsfAJziAjADYATAHZ5sqQBoQAT0SKyIgQBYBU2SakCBZBeIC+dragzCIjWAGMO7MB+aRMAGE8AEEiXAB9AHEQgFlcciokEHomVg4uXgRVWWFlRTlZRWNFeVU+LV0EcQFxYVlxcQMyVQL5KWUHJ3QwV3cvdh8-AIApAHkASQA5aLiEyi5UljZOZKymkTUzFVUyKTIVeUrEIzIjUSk1WWUjRT4jIykjLpBnXo8AJzAUVnYoKJ6mAgHF6jHYADc6ABrXpvYSfb6-f49BBgyEeH4rRKJRYMZYZNaIcTKXICcymZRWcRKKSKY7VUrCQS3WQHKyyPiKWQvOEIzF-AEYTBgD4fOgfYQ0AA2PwAZuK0MJeV9+ciMKiIXQMel2NiFskljrMvwBMo8pJ5MYynx5Lc6TpEJazdcbdtHtSDjyesIAFZ0MFgtVYADquAAQvgxoEANLECKBMZTKa4QJECK4bDYMbYHEGvFGwkIaxkYS3YmmqR8Rr7TQOovE0tkKkGB7iU72Ryvb1+gMCwGhiNR2Np+L4fAhKLzJK0fMrY0IAC0rPO13MygtHKK9LbhlZynylLJnO5nbhPfYgcFIfDkZjcdH48nJFk05Ss4JoCyC+MwgEXL2+5GPIgibvSQFSMI4iKEB+RyFYjxAV6LhSnQABGqHaJgA63sOEQPhOU64mkc6Fgutq5EB1JGKyNT7KU9K2iW1E2kU0HyPIkjPKe3ooehmHYUO94EI+CQvkR+KrJ+iALkBAj1NBpilMU5iWGB7SQWxsGyPBTwOJ27B0BAcBcG84kFlJi7ARBlHkRIpgHEcdZFCW9ymLIDTLpcMhIb0bieN4vj+BAZkkRZfDmMIihsWSEgHMogj0tY5wtGYFh3HcygtD58IqkiV4hR+PCIJSuT7hIUGmpanKOVU+RyfILR7DaaiSHc2XnpePQFZJRUIKccmWA0DwKLSRh8HwyjbmNv57ge7LHtlvEYd187fkUGlPKUlpFCYDRTYowjtPcBj3EBYidHpQA */
     id: 'game',
     predictableActionArguments: true,
     tsTypes: {} as import('./game.typegen').Typegen0,
@@ -54,7 +58,8 @@ const gameFsm = createMachine(
         | CreateGameEvent
         | JoinGameEvent
         | WebsocketConnectErrorEvent
-        | WebsocketMessageEvent,
+        | WebsocketMessageEvent
+        | ResetEvent,
       context: {} as Context,
       services: {} as {
         createGame: {
@@ -68,6 +73,9 @@ const gameFsm = createMachine(
       players: [],
     },
     initial: 'disconnected',
+    on: {
+      RESET: 'disconnected',
+    },
     states: {
       disconnected: {
         on: {
@@ -81,7 +89,6 @@ const gameFsm = createMachine(
           },
         },
       },
-
       creatingGame: {
         invoke: {
           src: 'createGame',
