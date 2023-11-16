5c2a8fee5eadbe378862529dc60c6ff27f054a6a
ortizdaniel <brisk.salt4034@fastmail.com>
2023-11-16 23:26:03 +0100

    feat: extract avatar to another component (#87)

--- src/components/PlayerList/Player/Player.tsx
+++ src/components/PlayerList/Player/Player.tsx
@@ -1,9 +1,8 @@
-/* eslint-disable @next/next/no-img-element */
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import { Flex, Text } from '@chakra-ui/react';
-import { thumbs } from '@dicebear/collection';
-import { createAvatar } from '@dicebear/core';
+
+import Avatar from './Avatar';
 
 import styles from './Player.module.scss';
 
@@ -13,31 +12,9 @@ type PlayerProps = {
 };
 
 const Player: React.FC<PlayerProps> = ({ nickname, isHost }) => {
-  const avatar: string = useMemo(
-    () =>
-      createAvatar(thumbs, {
-        seed: nickname,
-        backgroundColor: ['transparent'],
-        shapeOffsetX: [0],
-        shapeOffsetY: [0],
-        shapeRotation: [0],
-        faceOffsetY: [-10, 15],
-      }).toString(),
-    [nickname]
-  );
-
   return (
     <Flex className={styles.player}>
-      <div className={styles.avatarWrapper}>
-        <img // https://stackoverflow.com/q/44900569
-          className={styles.thumb}
-          src={`data:image/svg+xml;utf8,${encodeURIComponent(avatar)}`}
-          alt={nickname}
-        />
-        {isHost ? (
-          <img className={styles.crown} src='/svg/crown.svg' alt='crown' />
-        ) : null}
-      </div>
+      <Avatar isHost={isHost} nickname={nickname} />
       <Text className={styles.playerName}>{nickname}</Text>
     </Flex>
   );
