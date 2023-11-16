15d74696efd1d08c6a4146cb15bf09068bfff04e
ortizdaniel <brisk.salt4034@fastmail.com>
2023-11-16 21:16:03 +0100

    feat: consistent cards (#86)

--- src/components/PlayerList/PlayerList.tsx
+++ src/components/PlayerList/PlayerList.tsx
@@ -18,23 +18,13 @@ type PlayerListProps = {
 
 const PlayerList: React.FC<PlayerListProps> = ({ players }) => {
   return (
-    <Card size='sm' width='100%'>
-      <CardHeader paddingBottom={0}>
-        <Heading as='h4' textAlign='center' size='sm'>
-          Players ({players.length})
-        </Heading>
-        <Divider marginTop={'12px'} />
-      </CardHeader>
-      <CardBody>
-        <List spacing='12px'>
-          {players.map((player) => (
-            <ListItem key={player.nickname}>
-              <Player nickname={player.nickname} isHost={player.isHost} />
-            </ListItem>
-          ))}
-        </List>
-      </CardBody>
-    </Card>
+    <List spacing='12px'>
+      {players.map((player) => (
+        <ListItem key={player.nickname}>
+          <Player nickname={player.nickname} isHost={player.isHost} />
+        </ListItem>
+      ))}
+    </List>
   );
 };
 
