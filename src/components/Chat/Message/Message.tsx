6bbb4216050d1a538ab283e700584ad0aaa74b3f
Daniel <brisk.salt4034@fastmail.com>
2023-11-20 20:00:26 +0100

    feat: add crown to avatars in messages

--- src/components/Chat/Message/Message.tsx
+++ src/components/Chat/Message/Message.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
 
 import { HStack, Text } from '@chakra-ui/react';
+import { HtmlProps } from 'next/dist/shared/lib/html-context.shared-runtime';
 
 import Avatar from 'src/components/PlayerList/Player/Avatar';
 
@@ -11,11 +12,17 @@ export type MessageProps = {
   content: string;
 };
 
+const CROWN_STYLE: HTMLAttributes<HtmlProps>['style'] = {
+  width: '13.5px',
+  top: '-7px',
+  left: '5.15px',
+};
+
 const Message: React.FC<MessageProps> = ({ sender, content }) => {
   return (
     <>
       <HStack className={styles.messageContainer}>
-        <Avatar nickname={sender} size={24} />
+        <Avatar nickname={sender} size={24} crownStyle={CROWN_STYLE} />
         <Text className={styles.message}>
           <span className={styles.messageSender}>{sender}:</span> {content}
         </Text>
--- src/components/PlayerList/Player/Avatar/Avatar.tsx
+++ src/components/PlayerList/Player/Avatar/Avatar.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useMemo } from 'react';
+import React, { HTMLAttributes, useMemo } from 'react';
 
 import { thumbs } from '@dicebear/collection';
 import { createAvatar } from '@dicebear/core';
+import { HtmlProps } from 'next/dist/shared/lib/html-context.shared-runtime';
 
 import styles from './Avatar.module.scss';
 
@@ -10,9 +11,22 @@ export type AvatarProps = {
   nickname: string;
   isHost?: boolean;
   size?: number;
+  crownStyle?: HTMLAttributes<HtmlProps>['style'];
 };
 
-const Avatar: React.FC<AvatarProps> = ({ nickname, isHost, size }) => {
+const DEFAULT_CROWN_SIZE = 32;
+const DEFAULT_CROWN_STYLE: HTMLAttributes<HtmlProps>['style'] = {
+  width: '18px',
+  top: '-9px',
+  left: '7px',
+};
+
+const Avatar: React.FC<AvatarProps> = ({
+  nickname,
+  isHost,
+  size,
+  crownStyle,
+}) => {
   const avatar: string = useMemo(
     () =>
       createAvatar(thumbs, {
@@ -29,12 +43,17 @@ const Avatar: React.FC<AvatarProps> = ({ nickname, isHost, size }) => {
   return (
     <div className={styles.avatarWrapper}>
       <img // https://stackoverflow.com/q/44900569
-        width={size ?? 32}
+        width={size ?? DEFAULT_CROWN_SIZE}
         src={`data:image/svg+xml;utf8,${encodeURIComponent(avatar)}`}
         alt={nickname}
       />
       {isHost ? (
-        <img className={styles.crown} src='/svg/crown.svg' alt='crown' />
+        <img
+          className={styles.crown}
+          src='/svg/crown.svg'
+          alt='crown'
+          style={crownStyle ?? DEFAULT_CROWN_STYLE}
+        />
       ) : null}
     </div>
   );
