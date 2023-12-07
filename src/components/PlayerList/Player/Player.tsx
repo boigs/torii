f08350965930d85ffe2713419e974e7e2e698ac4
Daniel <brisk.salt4034@fastmail.com>
2023-12-07 22:27:35 +0100

    feat: reduced opacity for disconnected players

--- src/components/PlayerList/Player/Player.tsx
+++ src/components/PlayerList/Player/Player.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
-import { Flex, Text } from '@chakra-ui/react';
+import { Flex, Text, Tooltip } from '@chakra-ui/react';
+import classNames from 'classnames';
+
+import type { Player } from 'src/domain';
 
 import type { Player } from 'src/domain';
 
@@ -13,11 +16,26 @@ type PlayerProps = {
 };
 
 const Player: React.FC<PlayerProps> = ({ player }) => {
+  const disconnected = !player.isConnected;
+
   return (
-    <Flex className={styles.player}>
-      <Avatar player={player} />
-      <Text className={styles.playerName}>{player.nickname}</Text>
-    </Flex>
+    <Tooltip
+      placement='left'
+      hasArrow
+      label={disconnected ? `"${player.nickname}" has left` : null}
+    >
+      <Flex
+        className={classNames(
+          [styles.player],
+          disconnected ? styles.disconnected : null
+        )}
+      >
+        <>
+          <Avatar player={player} />
+          <Text className={styles.playerName}>{player.nickname}</Text>
+        </>
+      </Flex>
+    </Tooltip>
   );
 };
 
--- src/domain/player.ts
+++ src/domain/player.ts
@@ -1,6 +1,7 @@
 type Player = {
   nickname: string;
   isHost: boolean;
+  isConnected: boolean;
 };
 
 export default Player;
