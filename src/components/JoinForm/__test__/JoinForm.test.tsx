142bf8603c121a9caddb18727c4ba56520dcd76b
ortizdaniel <brisk.salt4034@fastmail.com>
2023-12-07 11:37:39 +0100

    chore: join form tests (#110)

--- src/components/JoinForm/__test__/JoinForm.test.tsx
+++ src/components/JoinForm/__test__/JoinForm.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
 
 import JoinForm from '../JoinForm';
 
@@ -21,5 +22,51 @@ describe('JoinForm...', () => {
     const gameIdInput = screen.queryByPlaceholderText('Game id');
 
     expect(gameIdInput).not.toBeVisible();
+  });
+
+  it('should make the button not say "Join" when the component is loading', () => {
+    render(<JoinForm loading={true} />);
+
+    const joinButton = screen.getByRole('button');
+
+    expect(joinButton.textContent).not.toBe('Join');
+  });
+
+  it('should call the callback function with the input of the user', async () => {
+    const onSubmit = jest.fn();
+    render(<JoinForm onSubmit={onSubmit} />);
+
+    const gameIdInput = screen.getByPlaceholderText('Game id');
+    const nicknameInput = screen.getByPlaceholderText('Nickname');
+    const joinButton = screen.getByRole('button');
+
+    await act(async () => {
+      fireEvent.change(gameIdInput, { target: { value: 'any-id' } });
+      fireEvent.change(nicknameInput, { target: { value: 'any-nickname' } });
+      fireEvent.click(joinButton);
+    });
+
+    expect(onSubmit).toHaveBeenCalledWith({
+      gameId: 'any-id',
+      nickname: 'any-nickname',
+    });
+  });
+
+  it('should call the callback function with the input of the user and the given game id as prop', async () => {
+    const onSubmit = jest.fn();
+    render(<JoinForm gameId='any-id' onSubmit={onSubmit} />);
+
+    const nicknameInput = screen.getByPlaceholderText('Nickname');
+    const joinButton = screen.getByRole('button');
+
+    await act(async () => {
+      fireEvent.change(nicknameInput, { target: { value: 'any-nickname' } });
+      fireEvent.click(joinButton);
+    });
+
+    expect(onSubmit).toHaveBeenCalledWith({
+      gameId: 'any-id',
+      nickname: 'any-nickname',
+    });
   });
 });
