fb0a35b5a530fa04044ac181a7512faaf8789f2e
Daniel <brisk.salt4034@fastmail.com>
2023-12-12 22:35:27 +0100

    chore: remove remote image host

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'api.dicebear.com',
-        port: '',
-      },
-    ],
-  },
   reactStrictMode: false,
   output: 'standalone',
 };
