00c9d1ba033b1109dd82435bfd3ca0afd02271a0
Sergi Valbuena Garrido <24233455+svalbuena@users.noreply.github.com>
2024-04-03 18:29:12 +0200

    Handle NotEnoughPlayers error (#128)
    Co-authored-by: ortizdaniel <brisk.salt4034@fastmail.com>

--- src/helpers/errorDisplay.ts
+++ src/helpers/errorDisplay.ts
@@ -6,6 +6,8 @@ export const headcrabErrorToString = (error: HeadcrabError): string => {
       return 'The game you are trying to join does not seem to exist.';
     case HeadCrabErrorType.PlayerAlreadyExists:
       return 'There is another player with that nickname already.';
+    case HeadCrabErrorType.NotEnoughPlayers:
+      return 'Not enough players to start the game, at least 3 are needed.';
     case HeadCrabErrorType.UnprocessableMessage:
       return 'This action cannot be processed at this moment.';
     case HeadCrabErrorType.Internal:
