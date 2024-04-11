3888eb3970f7272932df00a7bb98149e29fc94ea
ortizdaniel <brisk.salt4034@fastmail.com>
2024-04-11 22:56:58 +0200

    feat: add spinner while players wait (#132)

---
+++ src/components/NonAdminLobby/NonAdminLobby.tsx
@@ -0,0 +1,45 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Center,
+  Divider,
+  Heading,
+  Spinner,
+  Text,
+} from '@chakra-ui/react';
+
+import styles from './NonAdminLobby.module.scss';
+
+type NonAdminLobbyProps = {
+  className?: string;
+};
+
+const NonAdminLobby: React.FC<NonAdminLobbyProps> = ({ className }) => {
+  return (
+    <Card size='sm' className={className}>
+      <CardHeader>
+        <Heading as='h3' textAlign='center' size='md'>
+          Lobby
+        </Heading>
+        <Divider marginTop={'12px'} />
+      </CardHeader>
+      <CardBody className={styles.body}>
+        <Text align='center' className={styles.text}>
+          Please wait until the host starts the game.
+        </Text>
+        <Center>
+          <Spinner
+            thickness='4px'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+            speed='1.75s'
+          />
+        </Center>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default NonAdminLobby;
---
+++ src/components/NonAdminLobby/index.ts
@@ -0,0 +1,3 @@
+import NonAdminLobby from './NonAdminLobby';
+
+export default NonAdminLobby;
