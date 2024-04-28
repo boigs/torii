908aebdd077c74ba0668a38fb491b4c3ee767769
ortizdaniel <brisk.salt4034@fastmail.com>
2024-04-28 17:19:56 +0200

    feat/add voting card for host (#165)

--- src/components/VotingCard/VotingCard.tsx
+++ src/components/VotingCard/VotingCard.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import { Button, HStack, Text, VStack } from '@chakra-ui/react';
 
 import Card from 'src/components/Card';
+import Spinner from 'src/components/Spinner';
 import { Player, Round, Word } from 'src/domain';
 
 import styles from './VotingCard.module.scss';
@@ -30,37 +31,47 @@ const VotingCard = ({ round, player, onWordClicked }: MyWordsProps) => {
     setAnyButtonClicked(false);
   }, [round.score.currentWord]);
 
-  return round.score.currentPlayer === player.nickname ? null : (
+  return (
     <Card header={<Text>Voting Card</Text>}>
-      <VStack className={styles.wordsContainer}>
-        <Text className={styles.votingInstructions}>
-          From the words you submitted, click the word you think matches{' '}
-          <i>&quot;{round.score.currentWord}&quot;</i>
-        </Text>
-        <HStack className={styles.buttonsContainer}>
-          {submittedWords.map((submittedWord) => (
-            <Button
-              key={submittedWord.word}
-              isDisabled={submittedWord.isUsed}
-              onClick={() => onWordButtonClicked(submittedWord)}
-              isActive={
-                !submittedWord.isUsed &&
-                selectedWord?.word === submittedWord.word
-              }
-              colorScheme='blue'
-            >
-              {submittedWord.word}
-            </Button>
-          ))}
-        </HStack>
-        <Button
-          isActive={anyButtonClicked && selectedWord === null}
-          colorScheme='gray'
-          onClick={() => onWordButtonClicked(null)}
-        >
-          Skip
-        </Button>
-      </VStack>
+      {round.score.currentPlayer === player.nickname ? (
+        <VStack>
+          <Text className={styles.hostInstructions}>
+            Please wait while the players cast their votes for the words you
+            submitted
+          </Text>
+          <Spinner size='lg' />
+        </VStack>
+      ) : (
+        <VStack className={styles.wordsContainer}>
+          <Text className={styles.votingInstructions}>
+            From the words you submitted, click the word you think matches{' '}
+            <i>&quot;{round.score.currentWord}&quot;</i>
+          </Text>
+          <HStack className={styles.buttonsContainer}>
+            {submittedWords.map((submittedWord) => (
+              <Button
+                key={submittedWord.word}
+                isDisabled={submittedWord.isUsed}
+                onClick={() => onWordButtonClicked(submittedWord)}
+                isActive={
+                  !submittedWord.isUsed &&
+                  selectedWord?.word === submittedWord.word
+                }
+                colorScheme='blue'
+              >
+                {submittedWord.word}
+              </Button>
+            ))}
+          </HStack>
+          <Button
+            isActive={anyButtonClicked && selectedWord === null}
+            colorScheme='gray'
+            onClick={() => onWordButtonClicked(null)}
+          >
+            Skip
+          </Button>
+        </VStack>
+      )}
     </Card>
   );
 };
