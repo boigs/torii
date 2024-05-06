c23437dca967e4ccf806d56e67f03e01fd31af9d
ortizdaniel <brisk.salt4034@fastmail.com>
2024-05-06 21:26:39 +0200

    feat: split WordScores into Card and Modal (#198)

--- src/components/WordScores/WordScores.tsx
+++ src/components/WordScores/WordScores.tsx
@@ -8,65 +8,101 @@ import {
   Tooltip,
   VStack,
 } from '@chakra-ui/react';
+import classNames from 'classnames';
 
 import Card from 'src/components/Card';
+import { Modal, ModalBody, ModalHeader } from 'src/components/Modal';
 import { Player, Round, Word } from 'src/domain';
 
 import styles from './WordScores.module.scss';
 
-interface WordScores {
+interface WordScoresProps {
   player: Player;
   round: Round;
   className?: string;
 }
 
-const WordScores = ({ player, round, className }: WordScores) => {
+interface WordScoresModalProps extends Omit<WordScoresProps, 'className'> {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const WordScores = ({ player, round }: WordScoresProps) => {
   const words = round.getPlayerWords(player);
 
-  const currentlyVotingForMyWord = (votingWord: Word) => {
-    const votingItem = round.getVotingItem();
-    return votingItem.player === player && votingItem.word === votingWord.value;
-  };
-
-  const hasWordBeenUsed = (votingWord: Word) => {
-    return votingWord.isUsed && !currentlyVotingForMyWord(votingWord);
+  const hasWordBeenUsed = (word: Word) => {
+    // TODO also return false for the current voting item without throwing an NPE
+    return word.isUsed;
   };
 
   return (
+    <VStack>
+      {words.map((word, index) => (
+        <Flex key={index} className={styles.scoreContainer}>
+          <InputGroup>
+            <InputLeftAddon className={styles.wordLeftAddon}>
+              {index + 1}.
+            </InputLeftAddon>
+            <Input readOnly={true} className={styles.word} value={word.value} />
+          </InputGroup>
+          <Tooltip
+            placement='right'
+            hasArrow
+            label={hasWordBeenUsed(word) ? undefined : 'Not used in a vote yet'}
+          >
+            <Center className={styles.score}>
+              <Text>{hasWordBeenUsed(word) ? word.score : '-'}</Text>
+            </Center>
+          </Tooltip>
+        </Flex>
+      ))}
+    </VStack>
+  );
+};
+
+const WordScoresCard = ({ player, round, className }: WordScoresProps) => {
+  return (
     <Card header='Score' className={className}>
-      <VStack>
+      {round.getPlayerWords(player).length > 0 ? (
+        <>
+          <Text
+            className={classNames(styles.description, styles.cardDescription)}
+          >
+            These are the words you submitted and their current score on the
+            right.
+          </Text>
+          <WordScores player={player} round={round} />
+        </>
+      ) : (
         <Text className={styles.description}>
-          These are the words you submitted and their current score on the
-          right.
+          It seems like you did not submit any words, so there is no score
+          available.
         </Text>
-        {words.map((word, index) => (
-          <Flex key={index} className={styles.scoreContainer}>
-            <InputGroup>
-              <InputLeftAddon className={styles.wordLeftAddon}>
-                {index + 1}.
-              </InputLeftAddon>
-              <Input
-                readOnly={true}
-                className={styles.word}
-                value={word.value}
-              />
-            </InputGroup>
-            <Tooltip
-              placement='right'
-              hasArrow
-              label={
-                hasWordBeenUsed(word) ? undefined : 'Not used in a vote yet'
-              }
-            >
-              <Center className={styles.score}>
-                <Text>{hasWordBeenUsed(word) ? word.score : '-'}</Text>
-              </Center>
-            </Tooltip>
-          </Flex>
-        ))}
-      </VStack>
+      )}
     </Card>
   );
 };
 
-export default WordScores;
+const WordScoresModal = ({
+  player,
+  round,
+  isOpen,
+  onClose,
+}: WordScoresModalProps) => {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} showCloseButton={true}>
+      <ModalHeader>
+        {player.nickname}&apos;s Words on Round {round.index + 1}
+      </ModalHeader>
+      <ModalBody className={styles.modalBody}>
+        {round.getPlayerWords(player).length > 0 ? (
+          <WordScores player={player} round={round} />
+        ) : (
+          <Text className={styles.description}>No words were submitted.</Text>
+        )}
+      </ModalBody>
+    </Modal>
+  );
+};
+
+export { WordScoresModal, WordScoresCard };
--- src/components/WordScores/index.ts
+++ src/components/WordScores/index.ts
@@ -1,1 +1,1 @@
-export { default } from './WordScores';
+export { WordScoresModal, WordScoresCard } from './WordScores';
