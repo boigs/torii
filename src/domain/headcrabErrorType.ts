5fc03f98ddcbfb579a54825d81facc6ac4f6fe53
svalbuena <24233455+svalbuena@users.noreply.github.com>
2024-05-07 19:15:03 +0200

    Update errors

--- src/domain/headcrabErrorType.ts
+++ src/domain/headcrabErrorType.ts
@@ -1,13 +1,24 @@
 export enum HeadcrabErrorType {
-  Internal = 'Internal',
-  WebsocketClosed = 'WebsocketClosed',
-  UnprocessableMessage = 'UnprocessableMessage',
-  CommandNotAllowed = 'CommandNotAllowed',
-  NotEnoughPlayers = 'NotEnoughPlayers',
-  GameDoesNotExist = 'GameDoesNotExist',
-  PlayerAlreadyExists = 'PlayerAlreadyExists',
-  RepeatedWords = 'RepeatedWords',
+  // Domain
   GameAlreadyInProgress = 'GameAlreadyInProgress',
+  GameDoesNotExist = 'GameDoesNotExist',
+  InvalidStateForWordsSubmission = 'InvalidStateForWordsSubmission',
+  InvalidStateForVotingWordSubmission = 'InvalidStateForVotingWordSubmission',
+  NotEnoughPlayers = 'NotEnoughPlayers',
+  NotEnoughRounds = 'NotEnoughRounds',
+  NonHostPlayerCannotContinueToNextRound = 'NonHostPlayerCannotContinueToNextRound',
+  NonHostPlayerCannotContinueToNextVotingItem = 'NonHostPlayerCannotContinueToNextVotingItem',
+  NonHostPlayerCannotStartGame = 'NonHostPlayerCannotStartGame',
+  PlayerAlreadyExists = 'PlayerAlreadyExists',
+  PlayerCannotSubmitNonExistingOrUsedVotingWord = 'PlayerCannotSubmitNonExistingOrUsedVotingWord',
+  PlayerCannotSubmitVotingWordWhenVotingItemIsNone = 'PlayerCannotSubmitVotingWordWhenVotingItemIsNone',
+  RepeatedWords = 'RepeatedWords',
+  VotingItemPlayerCannotSubmitVotingWord = 'VotingItemPlayerCannotSubmitVotingWord',
+  // External
+  UnprocessableWebsocketMessage = 'UnprocessableMessage',
+  WebsocketClosed = 'WebsocketClosed',
+  // Internal
+  Internal = 'Internal',
 }
 
 export default HeadcrabErrorType;
