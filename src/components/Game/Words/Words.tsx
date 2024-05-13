fb8c42164fe0aa661baed47e36eec6e36853a3a0
Daniel <brisk.salt4034@fastmail.com>
2024-05-13 21:40:13 +0200

    feat: place WordsInput in grid

---
+++ src/components/Game/Words/Words.tsx
@@ -0,0 +1,33 @@
+import classNames from 'classnames';
+
+import { useGameContext } from 'src/components/GameContextProvider';
+import { artificialSleep } from 'src/helpers/sleep';
+import { playerWords } from 'src/websocket/out';
+
+import WordsInput from './WordsInput';
+
+import styles from './Words.module.scss';
+
+interface WordsProps {
+  className?: string;
+}
+
+const Words = ({ className }: WordsProps) => {
+  const { game, sendWebsocketMessage } = useGameContext();
+
+  const sendPlayerWords = async (words: string[]) => {
+    sendWebsocketMessage(playerWords({ words }));
+    await artificialSleep(350);
+  };
+
+  return (
+    <WordsInput
+      player={game.player}
+      round={game.lastRound()}
+      onSubmit={sendPlayerWords}
+      className={classNames(className, styles.wordsInput)}
+    />
+  );
+};
+
+export default Words;
