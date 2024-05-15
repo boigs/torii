27426795af47d54e6d877e3d810138511b82a06e
Daniel <brisk.salt4034@fastmail.com>
2024-05-15 23:18:30 +0200

    feat: use bullet points in instructions

--- src/components/game/Lobby/Instructions/Instructions.tsx
+++ src/components/game/Lobby/Instructions/Instructions.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@chakra-ui/react';
+import { ListItem, Text, UnorderedList } from '@chakra-ui/react';
 
 import Card from 'src/components/shared/Card';
 
@@ -11,13 +11,20 @@ interface InstructionsProps {
 const Instructions = ({ className }: InstructionsProps) => {
   return (
     <Card header='How to Play' className={className}>
-      <Text className={styles.instructions}>
-        In each round, you will be presented with a random topic, you must write
-        up to 8 words related to that topic. But beware! You should try to match
-        the answers of as many participants as possible. After all rounds are
-        over, whoever has matched the most answers with others will win the
-        game.
-      </Text>
+      <UnorderedList className={styles.instructions} stylePosition='inside'>
+        <ListItem>
+          Each round will present a random topic, and you must{' '}
+          <b>write up to 8 words related to that topic</b>.
+        </ListItem>
+        <ListItem>
+          Try to write words you think will{' '}
+          <b>match other participant&quot;s words</b>.
+        </ListItem>
+        <ListItem>
+          After all rounds are over, whoever has{' '}
+          <b>matched more words with others wins</b> the game.
+        </ListItem>
+      </UnorderedList>
     </Card>
   );
 };
