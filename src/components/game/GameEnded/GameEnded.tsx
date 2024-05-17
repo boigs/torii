2f74241c52675e965a0d348aff05fd9927473088
Daniel <brisk.salt4034@fastmail.com>
2024-05-18 00:27:31 +0200

    feat: add end of game component

---
+++ src/components/game/GameEnded/GameEnded.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+
+import { useGameContext } from 'src/components/context/GameContextProvider';
+
+import Scoreboard from './Scoreboard';
+
+import styles from './GameEnded.module.scss';
+
+interface GameEndedProps {
+  className?: string;
+}
+
+const GameEnded = ({ className }: GameEndedProps) => {
+  const { game } = useGameContext();
+
+  return (
+    <div className={classNames(className, styles.gameEndedContainer)}>
+      <Scoreboard players={game.players} rounds={game.rounds} />
+    </div>
+  );
+};
+
+export default GameEnded;
