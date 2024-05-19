85cc3a6d5577822088800faabbb14a998f2a859c
Daniel <brisk.salt4034@fastmail.com>
2024-05-20 01:06:44 +0200

    feat: migrate create and join pages to react router

--- src/pages/join.tsx
+++ src/pages/join.tsx
@@ -1,37 +1,29 @@
-'use client';
-
 import { useEffect } from 'react';
 
 import { Center } from '@chakra-ui/react';
-import { useRouter } from 'next/navigation';
+import { useParams } from 'react-router-dom';
 
 import { useGameContext } from 'src/components/context/GameContextProvider';
 import JoinGameForm from 'src/components/joinGame/JoinForm';
 
-interface JoinQuery {
-  params: {
-    id?: string[]; // single optional path params are not supported yet by NextJS
-  };
-}
-
-const Join = ({ params: { id } }: JoinQuery) => {
-  const router = useRouter();
+const Join = () => {
+  // const router = useRouter();
+  const { gameId } = useParams();
   const { gameConnectionActor } = useGameContext();
   const [gameConnection] = [gameConnectionActor.getSnapshot()];
-  const realId = id?.[0];
 
   useEffect(() => {
     if (gameConnection.context.gameJoined) {
       gameConnectionActor.send({ type: 'RESET' });
     } else if (gameConnection.matches('game')) {
-      router.push('/game');
+      // router.push('/game');
     }
-  }, [gameConnection, router, gameConnectionActor]);
+  }, [gameConnection, /*router,*/ gameConnectionActor]);
 
   return (
     <Center>
       <JoinGameForm
-        gameId={realId}
+        gameId={gameId}
         loading={!gameConnection.matches('disconnected')}
         onSubmit={({ gameId, nickname }) => {
           gameConnectionActor.send({ type: 'JOIN_GAME', gameId, nickname });
