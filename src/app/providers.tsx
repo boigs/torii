86769a074ef36f27d962626edbe061c9a4eefc74
Daniel <brisk.salt4034@fastmail.com>
2024-05-20 00:32:47 +0200

    chore: remove next provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,10 +1,5 @@
-'use client';
-
-import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 
 export const Providers = ({ children }: { children: React.ReactNode }) => (
-  <CacheProvider>
-    <ChakraProvider>{children}</ChakraProvider>
-  </CacheProvider>
+  <ChakraProvider>{children}</ChakraProvider>
 );
