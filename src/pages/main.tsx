15a24820847ff20b53c1ea0056dd7cf0466bbedb
ortizdaniel <brisk.salt4034@fastmail.com>
2024-05-20 11:47:54 +0200

    feat: create main page (#277)

---
+++ src/pages/main.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import { Providers } from 'src/app/providers';
+import { GameContextProvider } from 'src/components/context/GameContextProvider';
+import AnimatedParent from 'src/components/shared/AnimatedParent';
+
+import Create from './create';
+import Join from './join';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Create />,
+  },
+  {
+    path: 'join/:gameId?',
+    element: <Join />,
+  },
+]);
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Providers>
+      <GameContextProvider>
+        <AnimatedParent>
+          <RouterProvider router={router} />
+        </AnimatedParent>
+      </GameContextProvider>
+    </Providers>
+  </React.StrictMode>,
+);
