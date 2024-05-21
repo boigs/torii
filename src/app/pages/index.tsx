80fbc7e00746e6eb2904c09a6d5382871aab1503
Daniel <brisk.salt4034@fastmail.com>
2024-05-21 20:40:01 +0200

    chore: remove non-null assertion

--- src/app/pages/index.tsx
+++ src/app/pages/index.tsx
@@ -26,8 +26,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
     <ChakraProvider>
       <GameContextProvider>
