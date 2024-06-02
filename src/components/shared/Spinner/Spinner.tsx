162aaa6abef5d68318ece45fa69f877238dae471
ortizdaniel <brisk.salt4034@fastmail.com>
2024-06-02 23:23:17 +0200

    feat: add done indicator for word submission (#307)

--- src/components/shared/Spinner/Spinner.tsx
+++ src/components/shared/Spinner/Spinner.tsx
@@ -2,15 +2,17 @@ import { Spinner as ChakraSpinner, ResponsiveValue } from '@chakra-ui/react';
 
 interface SpinnerProps {
   size?: ResponsiveValue<string>;
+  className?: string;
 }
 
-const Spinner = ({ size }: SpinnerProps) => (
+const Spinner = ({ size, className }: SpinnerProps) => (
   <ChakraSpinner
     thickness='4px'
     emptyColor='gray.200'
     color='blue.500'
     size={size ?? 'xl'}
     speed='1.75s'
+    className={className}
   />
 );
 
